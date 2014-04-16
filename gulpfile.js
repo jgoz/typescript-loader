144f0b58a41f878798cf8caa2de6c6c0c4ece8c7
John Gozde <johng@pandell.com>
2014-04-16 16:28:46 -0600

    Add dependencies and initial gulpfile.

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+"use strict";
+
+var glob = require("glob");
+var gulp = require("gulp");
+var mocha = require("gulp-mocha");
+var jshint = require("gulp-jshint");
+var path = require("path");
+var spawn = require("child_process").spawn;
+
+//-------------------------------------------
+// Perform static analysis on project sources.
+gulp.task("jshint", function () {
+    return gulp.src(["*.js", "src/*.js", "tests/*.js"])
+        .pipe(jshint())
+        .pipe(jshint.reporter("jshint-stylish"));
+});
+
+//-------------------------------------------
+// Execute unit tests.
+gulp.task("test:unit", function () {
+    return gulp.src("./tests/*.js").pipe(mocha());
+});
+
+//-------------------------------------------
+// Execute integration tests.
+gulp.task("test:integration", function () {
+    return gulp.src("./tests/integration/*.js").pipe(mocha());
+});
+
+//-------------------------------------------
+// Build project for distribution
+gulp.task("build", function (done) {
+    var typescriptPath = path.resolve(require.resolve("typescript"));
+    var tscpath = path.join(path.dirname(typescriptPath), "tsc");
+
+    var args = [tscpath];
+    args.push("--module", "commonjs");
+    args.push("--noImplicitAny");
+    args.push("--target", "ES5");
+    args.push("--out", "dist/index.js");
+    args.push.apply(args, glob.sync("./*.ts"));
+    args.push.apply(args, glob.sync("./src/*.ts"));
+
+    console.log(tscpath);
+    console.log(args.join(" "));
+
+    var tsc = spawn("node", args, { stdio: "inherit" });
+    tsc.on("exit", function (code) {
+        done(code === 0 ? undefined : "Typescript compilation failed.");
+    });
+    tsc.on("error", function (err) {
+        done(err);
+    })
+});
+
+gulp.task("test", ["test:unit", "test:integration"]);
+gulp.task("default", ["jshint", "test"]);
