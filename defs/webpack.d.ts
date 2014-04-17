42796e2d59779d2fb083fc5c6339d79cec859d6f
John Gozde <johng@pandell.com>
2014-04-16 18:11:32 -0600

    Add typescript definitions.

---
+++ defs/webpack.d.ts
@@ -0,0 +1,58 @@
+/// <reference path="node/node.d.ts" />
+
+declare module "webpack" {
+    export interface Loader {
+        request: string;
+        path: string;
+        query: string;
+    }
+
+    export interface LoaderContext {
+        version: number;
+        context: string;
+        request: string;
+        query: string;
+        data: Object;
+        loaders: Array<Loader>;
+        loaderIndex: number;
+        resource: string;
+        resourcePath: string;
+        resourceQuery: string;
+        values: Array<any>;
+        inputValues: Array<any>;
+
+        options: Object;
+        debug: boolean;
+        minimize: boolean;
+        target: string;
+        webpack: boolean;
+
+        async(flag?: boolean): void;
+        cacheable(flag?: boolean): void;
+        emitError(message: string): void;
+        emitWarning(message: string): void;
+        exec(code: string, filename: string): void;
+        resolve(context: string, request: string, callback: (err: Object, result: string) => void): void;
+        resolveSync(context: string, request: string): string;
+        addDependency(file: string): void;
+        dependency(file: string): void;
+        addContextDependency(directory: string): void;
+        clearDependencies(): void;
+        emitFile(name: string, content: string, sourceMap?: Object): void;
+        emitFile(name: string, content: NodeBuffer, sourceMap?: Object): void;
+        callback(error: any, content: string, sourceMap?: Object): void;
+        callback(error: any, content: NodeBuffer, sourceMap?: Object): void;
+    }
+
+    export interface LoaderPlugin extends LoaderContext {
+        (source: string): void;
+    }
+}
+
+declare module "loader-utils" {
+    import webpack = require("webpack");
+
+    export function parseQuery(query: string): Object;
+    export function getCurrentRequest(loaderContext: webpack.LoaderContext): string;
+    export function getRemainingRequest(loaderContext: webpack.LoaderContext): string;
+}
