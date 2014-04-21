12ab6a921ccf2d446f93c9062ddd3f0599517c9e
John Gozde <john@gozde.ca>
2014-04-20 20:25:16 -0600

    Add 'watch' task.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var glob = require("glob");
 var gulp = require("gulp");
+var gutil = require("gulp-util");
 var mocha = require("gulp-mocha");
 var jshint = require("gulp-jshint");
 var path = require("path");
@@ -27,9 +28,7 @@ gulp.task("test:integration", function () {
     return gulp.src("./tests/integration/*.js").pipe(mocha());
 });
 
-//-------------------------------------------
-// Build project for distribution
-gulp.task("build", function (done) {
+function build(watch, done) {
     var typescriptPath = path.resolve(require.resolve("typescript"));
     var tscpath = path.join(path.dirname(typescriptPath), "tsc");
 
@@ -38,17 +37,36 @@ gulp.task("build", function (done) {
     args.push("--noImplicitAny");
     args.push("--target", "ES5");
     args.push("--outDir", "dist");
-    args.push("--out", "index.js");
+    if (watch) {
+        args.push("--watch");
+    }
     args.push.apply(args, glob.sync("./*.ts"));
     args.push.apply(args, glob.sync("./src/*.ts"));
 
     var tsc = spawn("node", args, { stdio: "inherit" });
     tsc.on("exit", function (code) {
-        done(code === 0 ? undefined : "Typescript compilation failed.");
+        if (code !== 0) { gutil.beep(); }
+        if (!watch) { done(code === 0 ? undefined : "Typescript compilation failed."); }
     });
     tsc.on("error", function (err) {
-        done(err);
+        if (watch) {
+            gutil.log(err);
+        } else {
+            done(err);
+        }
     });
+}
+
+//-------------------------------------------
+// Build project for distribution
+gulp.task("build", function (done) {
+    build(false, done);
+});
+
+//-------------------------------------------
+// Watch project sources
+gulp.task("watch", function (done) {
+    build(true, done);
 });
 
 gulp.task("test", ["test:unit", "test:integration"]);
